68a5ac34267a48fd1e02b8ef14c1442bf55c9ee0
FrederickKisife <kilafrederick@gmaul.com>
2023-03-20 12:41:15 -0700

    creating different pages and styling them

---
+++ screens/home.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Home() {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home Screen</Text>
+    </View>
+  );
+}
+
---
+++ styles/global.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    titleText:{
+       fontWeight:'bold',
+       textAlign: 'center',
+       fontSize: 18,
+       marginTop: 16,
+       padding:24,
+       color: '#333',
+    },
+    paragraph:{
+        marginVertical: 8,
+        lineHeight: 20,
+    }
+})
